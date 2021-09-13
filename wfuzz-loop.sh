8d72229b093723326d77c9924725ea698deb0107
FlyingPhish <46652779+FlyingPhish@users.noreply.github.com>
2021-09-13 16:59:12 +0100

    Upload quick n dirty wfuzz loop script

---
+++ wfuzz-loop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# USAGE
+# wfuzz-loop.sh path/to/wordlist https://mytarget.com
+
+printf "\nRunning WFuzz with word list $1 against $2. Results saved to wfuzz-list-output.txt"
+echo ""
+while read -r url; do wfuzz --script=listing,robots,sitemap -c -L --no-cache --hc 418,404,403,401,301 -w $1 $url/FUZZ | tee -a wfuzz-list-output.txt; done < $2
